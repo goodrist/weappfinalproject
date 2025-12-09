ca8bae5810f1a1f5abb0ad678d30235718351be4
Stella Goodrich <goodrist@mail.gvsu.edu>
2025-12-08 20:22:40 -0500

    fixing

--- src/router.ts
+++ src/router.ts
@@ -1,15 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-import HomePage from './views/HomePage.vue';
-import WomensShopPage from './views/WomensShopPage.vue';
-import AccessoriesProducts from './views/AccessoriesProducts.vue';  // ✅ add this
+import HomePage from '../views/HomePage.vue';
+import WomensShopPage from '../views/WomensShopPage.vue';
+import AccessoriesProducts from '../views/AccessoriesProducts.vue';
 
 const routes = [
   { path: '/', component: HomePage },
   { path: '/women', component: WomensShopPage },
-
-  
-  { path: '/accessories', component: AccessoriesProducts },
+  { path: '/accessories', component: AccessoriesProducts }
 ];
 
 const router = createRouter({
