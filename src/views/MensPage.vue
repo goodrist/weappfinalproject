92bf283cdb5efcabed374a17776299f8d354f96b
Stella Goodrich <goodrist@mail.gvsu.edu>
2025-12-10 00:38:15 -0500

    fixing mens page featured

--- src/views/MensPage.vue
+++ src/views/MensPage.vue
@@ -12,7 +12,7 @@
   <ProductGrid
     v-if="!selectedItem"
     title="Featured Products"
-    :items="featuredProducts.slice(3, 6)"
+    :items="featuredProducts"
     @select="handleSelect"
   />
 
@@ -31,15 +31,11 @@ import { useRouter } from "vue-router";
 
 import ProductGrid from '../components/ProductGrid.vue';
 import ProductDetail from '../components/ProductDetail.vue';
-import pennantcrew from '../images/pennantcrew.jpg';
-import phoenixfleececrew from '../images/phoenixfleececrew.jpg';
-import powerblendsparklescrew from '../images/powerblendsparklescrew.jpg';
+
 import clubfleecehood from '../images/clubfleecehood.jpg';
 import heavyweighthood from '../images/heavyweighthood.jpg';
 import jerseylongsleevetee from '../images/jerseylongsleevetee.jpg';
-import riseswooshflexhat from '../images/riseswooshflexhat.jpg';
-import gvbeanie from '../images/gvbeanie.jpg';
-import jvstjohnsburytotebag from '../images/jvstjohnsburytotebag.jpg'; 
+
 
 const router = useRouter();
 
@@ -58,33 +54,6 @@ const onShopClick = () => {
 };
 
 const featuredProducts: Product[] = [
-  {
-    id: 'pennant-crew',
-    name: 'Pennant Crew',
-    price: '$49.99',
-    image: pennantcrew,
-    tag: 'Crewneck',
-    colors: ['Heather Gray', 'Royal Blue'],
-    sizes: ['S', 'M', 'L', 'XL'],
-  },
-  {
-    id: 'phoenix-fleece',
-    name: 'Phoenix Fleece Crew',
-    price: '$49.99',
-    image: phoenixfleececrew,
-    tag: 'Crewneck',
-    colors: ['Heather Gray'],
-    sizes: ['XS', 'S', 'M', 'L', 'XL'],
-  },
-  {
-    id: 'sparkle-crew',
-    name: 'Powerblend Sparkle Crew',
-    price: '$52.99',
-    image: powerblendsparklescrew,
-    tag: 'Crewneck',
-    colors: ['Royal Blue'],
-    sizes: ['S', 'M', 'L', 'XL'],
-  },
   {
     id: 'club-fleece',
     name: 'Club Fleece Hoodie',
@@ -111,33 +80,6 @@ const featuredProducts: Product[] = [
     tag: 'T-Shirt',
     colors: ['White', 'Royal Blue'],
     sizes: ['XS', 'S', 'M', 'L', 'XL'],
-  },
-  {
-    id: 'rise-swoosh',
-    name: 'Rise Swoosh Flex Hat',
-    price: '$29.99',
-    image: riseswooshflexhat,
-    tag: 'Hat',
-    colors: ['Royal Blue'],
-    sizes: ['One Size'],
-  },
-  {
-    id: 'gv-beanie',
-    name: 'GVSU Beanie',
-    price: '$19.99',
-    image: gvbeanie,
-    tag: 'Beanie',
-    colors: ['Royal Blue'],
-    sizes: ['One Size'],
-  },
-  {
-    id: 'jv-tote',
-    name: 'JV St. Johnsbury Tote Bag',
-    price: '$34.99',
-    image: jvstjohnsburytotebag,
-    tag: 'Accessory',
-    colors: ['Natural Canvas'],
-    sizes: ['One Size'],
   },
 ];
 
